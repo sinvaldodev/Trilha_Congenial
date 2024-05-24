0085c0e8d53a103f40cb034d01124b596afd95e6
Sinvaldo Pissinatti Silva <00851619266@aj.local>
2024-05-24 12:53:09 -0400

    Adicionando dois novos desafios e suas soluções

--- Vetores/main.cpp
+++ Vetores/main.cpp
@@ -8,10 +8,20 @@ int main () {
   int tamanho;
 
   cout << "Digite quantas notas você quer armazenar" << endl;
-
   cin >> tamanho;
 
   float notas[tamanho];
+
+  // lendo as notas dinamicamente
+  for (int i = 0; i < tamanho; i++) {
+    cout << "Coloque a nota " << i + 1 << ":" << endl;
+    cin >> notas[i];
+  }
+
+  // mostrando e printando as notas
+  for (int cont = 0; cont < tamanho; cont++) {
+    cout << "Essas são as notas: " << notas[cont] << endl;
+  }
 
 
   return 0;
